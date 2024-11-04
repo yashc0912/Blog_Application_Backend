f6ab8356dceeb51a573f71b9ee6f97301aa69f61
Mahesh Choubey <yash.mahesh.choubey@accenture.com>
2024-11-04 19:09:31 +0530

    Created new entity Post, and mapped entities Post, User, and Category with eachouther using ManyToOne and OneToMany relationship

---
+++ src/main/java/com/yash/blog/entities/Post.java
@@ -0,0 +1,47 @@
+package com.yash.blog.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer postId;
+
+	@Column(name = "post_title", length = 100, nullable = false)
+	private String title;
+
+	@Column(length = 1000000000)
+	private String content;
+
+	private String imageName;
+
+	private Date addedDate;
+
+	@ManyToMany
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	@ManyToOne
+	private User user;
+	
+
+}
--- src/main/java/com/yash/blog/entities/User.java
+++ src/main/java/com/yash/blog/entities/User.java
@@ -6,11 +6,15 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -37,6 +41,8 @@ public class User  {
 
 	private String about;
 
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Post> posts = new ArrayList<>();
 	
 
 }
